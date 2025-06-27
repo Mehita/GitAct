810a848fefa5dc879987f987db6e96109b9763b9
mehita fanny <mehita.fanny@ynov.com>
2025-06-27 12:09:14 +0200

    soustraction

--- maths.py
+++ maths.py
@@ -4,12 +4,23 @@ class SimpleMath:
     @staticmethod
     def addition(a, b):
         return a + b
+    
+    @staticmethod
+    def soustraction(a, b):
+        return a - b
 
 class TestSimpleMath(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(SimpleMath.addition(2, 3), 5)
         self.assertEqual(SimpleMath.addition(-1, 1), 0)
         self.assertEqual(SimpleMath.addition(0, 0), 0)
+
+    def test_soustraction(self):
+        self.assertEqual(SimpleMath.soustraction(5, 3), 2)
+        self.assertEqual(SimpleMath.soustraction(0, 0), 0)
+        self.assertEqual(SimpleMath.soustraction(-1, -1), 0)
+        self.assertEqual(SimpleMath.soustraction(-2, 3), -5)
+
 
 if __name__ == "__main__":
     unittest.main()
