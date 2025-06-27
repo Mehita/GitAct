c32c52c82bca4a0ae199d9bee2411dd6dd20c8d4
mehita fanny <mehita.fanny@ynov.com>
2025-06-27 11:50:18 +0200

    maths

---
+++ maths.py
@@ -0,0 +1,15 @@
+import unittest
+
+class SimpleMath:
+    @staticmethod
+    def addition(a, b):
+        return a + b
+
+class TestSimpleMath(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(SimpleMath.addition(2, 3), 5)
+        self.assertEqual(SimpleMath.addition(-1, 1), 0)
+        self.assertEqual(SimpleMath.addition(0, 0), 0)
+
+if __name__ == "__main__":
+    unittest.main()
